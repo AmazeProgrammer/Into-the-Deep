2688c229a725af15cb0bfdb0a69b5ef67343fa08
AmazeProgrammer <gavinsuaresb@gmail.com>
2025-01-19 15:12:15 -0600

    Merging new  PedroPathing

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -5,14 +5,14 @@ import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
+        ThreeWheelConstants.forwardTicksToInches = 0.0020107186487785013;
+        ThreeWheelConstants.strafeTicksToInches = 0.002028160219112976;
+        ThreeWheelConstants.turnTicksToInches = 0.002028161481753881;
+        ThreeWheelConstants.leftY = 6.1850394;
+        ThreeWheelConstants.rightY = -6.1692913;
+        ThreeWheelConstants.strafeX = -6.6181102;
         ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "leftBack";
         ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
