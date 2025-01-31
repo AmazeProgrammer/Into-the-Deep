efbb08f7f59b5fe6fb0d6f88d2ce42f0aab62cb9
Gavin Suares <gavinsuaresb@gmail.com>
2025-01-31 16:38:07 -0600

    Working code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/servoPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/servoPositions.java
@@ -4,16 +4,16 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class servoPositions {
-    public static double frontServoRightOpen = 0;
-    public static double frontServoLeftOpen = 0;
-    public static double frontServoRightClose = 0;
-    public static double frontServoLeftClose = 0;
-    public static double backServoRightOpen = 0;
-    public static double backServoLeftOpen = 0;
+    public static double frontServoRightOpen = 0.63;
+    public static double frontServoLeftOpen = 0.5;
+    public static double frontServoRightClose = 0.49;
+    public static double frontServoLeftClose = 0.34;
+    public static double backServoRightOpen = 0.1;
+    public static double backServoLeftOpen = 0.48;
     public static double backServoRightClose = 0;
-    public static double backServoLeftClose = 0;
+    public static double backServoLeftClose = 0.37;
     public static double turnFrontServoDown = 0;
-    public static int turnFrontServoUp = 0;
+    public static double turnFrontServoUp = 0;
     public static int turnBackServoSpecimenBack = 0;
     public static int turnBackServoSpecimenFront = 0;
     public static int turnBackServoBucket = 0;
@@ -28,9 +28,9 @@ public class servoPositions {
     public static int tickRotation3 = 4200;
     public static int tickRotation2 = 6000;
     //Reverse
-    public static boolean frontServoRightReverse = false;
+    public static boolean frontServoRightReverse = true;
     public static boolean frontServoLeftReverse = false;
-    public static boolean backServoRightReverse = false;
+    public static boolean backServoRightReverse = true;
     public static boolean backServoLeftReverse = false;
     public static boolean turnFrontServoReverse = false;
     public static boolean turnBackServoReverse = false;
